9d4f98baf0d98c857512d182da4175206e187c3f
Samitha Kuruvitaarachchi <samitha@amplifyn.com>
2023-06-16 00:07:08 +0530

    Login View added

---
+++ ecom/lib/views/MainHome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MainHome extends StatefulWidget {
+  const MainHome({Key? key}) : super(key: key);
+
+  @override
+  State<MainHome> createState() => _MainHomeState();
+}
+
+class _MainHomeState extends State<MainHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
